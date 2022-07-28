d0760b96f60591020e754d8a19def408f6fb675e
Pedro4064 <pedrohlcruz@gmail.com>
2022-07-28 14:57:06 -0300

    [IMPLEMENTED] Day of Week traits to be used with add rule method

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,49 @@
+#[repr(u8)]
+#[derive(Debug)]
 enum DayOfWeek {
     Monday = 0,
-    Tuesday = 1,
-    Wednesday = 2,
-    Thursday = 3,
-    Friday = 4,
-    Saturday = 5,
-    Sunday = 6,
+    Tuesday ,
+    Wednesday,
+    Thursday ,
+    Friday ,
+    Saturday ,
+    Sunday ,
 }
 
-//Basically every numerical entry on a cronjob can have *(for any) ,(for discrete separation) -(for
-//range) /(for step value)
+impl Into<u8> for DayOfWeek{
+    fn into(self) -> u8{
+        self as u8
+    }
+
+}
+
 #[derive(Debug)]
-enum Rule {
-    All,           // All is a simple *
-    Range(u8, u8), // A range is composed of 2 numerical values (e.g 0-15)
-    Step(u8),      // A step is composed of one number (e.g */3)
-    Unit(u8),      // A Unit is a simple numerical value
+enum Rule<T: Into<u8>> {
+    All,                    // All is a simple *
+    Range(T, T),          // A range is composed of 2 numerical values (e.g 0-15)
+    Step(T),               // A step is composed of one number (e.g */3)
+    Unit(T),               // A Unit is a simple numerical value
+    RangedStep(T, T, u8), // A RangedStep is a concatenation of a Range rule and and step (e.g 0-10/2 i.e every two minutes from mimute 0 to minute 10)
 }
 
 #[derive(Debug)]
 struct CronJob {
-    minute: Vec<Rule>,
-    hour: Vec<Rule>,
-    day_of_month: Vec<Rule>,
-    month: Vec<Rule>,
-    day_of_week: Vec<Rule>,
+    minute: Vec<Rule<u8>>,
+    hour: Vec<Rule<u8>>,
+    day_of_month: Vec<Rule<u8>>,
+    month: Vec<Rule<u8>>,
+    day_of_week: Vec<Rule<DayOfWeek>>,
     command: String,
 }
 
 impl CronJob {
     fn new() -> Self {
         CronJob {
-            minute: Vec::<Rule>::new(),
-            hour: Vec::<Rule>::new(),
-            day_of_week: Vec::<Rule>::new(),
-            month: Vec::<Rule>::new(),
-            day_of_month: Vec::<Rule>::new(),
+            minute: Vec::<Rule<u8>>::new(),
+            hour: Vec::<Rule<u8>>::new(),
+            day_of_week: Vec::<Rule<DayOfWeek>>::new(),
+            month: Vec::<Rule<u8>>::new(),
+            day_of_month: Vec::<Rule<u8>>::new(),
             command: String::new(),
         }
     }
@@ -47,19 +55,23 @@ impl CronJob {
     fn generate_file(&self, file_name: &str) {}
 }
 
-trait RuleManipulation {
-    fn add_rule(&mut self, new_rule: Rule);
+trait RuleManipulation <T: Into<u8>> {
+    fn add_rule(&mut self, new_rule: Rule<T>);
 }
 
-impl RuleManipulation for Vec<Rule> {
-    fn add_rule(&mut self, new_rule: Rule){
+impl <T: Into<u8>> RuleManipulation<T> for Vec<Rule<T>> {
+    fn add_rule(&mut self, new_rule: Rule<T>) {
         self.push(new_rule);
     }
 }
 
 fn main() {
     let mut anime = CronJob::new();
-    anime.minute.add_rule(Rule::Range(0, 10));
+    anime.minute.add_rule(Rule::Range(0, 10)); // Add rule to run job for every minute from 0 to 10
+    anime.hour.add_rule(Rule::RangedStep(0, 12, 2)); // Add rule to run job every two hours from hour 0 to hout 12
+    anime.day_of_week.add_rule(Rule::RangedStep(DayOfWeek::Monday, DayOfWeek::Wednesday, 2)); // Add rule to run job every two hours from hour 0 to hout 12
+
+
 
     println!("{:#?}", anime);
 }
